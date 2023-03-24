4f77f7b0e55690579d631e35ef3a5e3991eb8ab6
Pixelshot <pixelshot8@gmail.com>
2023-03-24 21:27:02 +0800

    Need to create feed route and assign card properly. Design is almost complete.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,26 +9,23 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, imageSrc, description }) => {
   return (
-    <div className="mx-auto flex flex-col justify-around border rounded-lg shadow md:flex-row md:max-w-xl bg-[#16181a] hover:shadow-xl">
-      <div className="p-4 border-2 border-red-400">
-        {/* <div className="p-4 border-solid border-2 border-sky-500"> */}
+    <div className="m-12 rounded-lg bg-[url('/noise.png')] bg-gray-800 shadow-xl grid sm:grid-cols-2">
+      <div className="p-4">
         <img
           src={imageSrc}
           alt={title}
-          className="rounded-t-lg mx-auto h-auto w-auto md:w-48 lg:w-96 md:rounded-none md:rounded-l-lg hover:shadow-xl"
+          className="rounded-t-lg mx-auto h-48 min-w-sm w-[100vh] object-cover md:rounded-none md:rounded-l-lg hover:shadow-xl"
         />
       </div>
       <div className="flex flex-col justify-between p-4 leading-normal">
-        <div className="flex flex-row items-center justify-between">
-          <h2 className="mb-2 text-2xl font-bold tracking-tight text-white">
+        <div className="flex flex-row justify-between">
+          <h2 className="mb-2 text-xl font-bold tracking-tight text-white">
             {title}
           </h2>
           <AiOutlineHeart color="white" size={20} />
         </div>
         {description && (
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {description}
-          </p>
+          <p className="mb-3 font-normal text-gray-100">{description}</p>
         )}
       </div>
     </div>
